c01b25350d03d82ed6a5f2f4936c23f0ed751ddc
eltonff <eltonfabricio10@gmail.com>
2021-10-05 13:42:47 -0300

    ok

---
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/test2.py
@@ -0,0 +1,25 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QFileDialog, QApplication
+import sys
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.btn = QPushButton('Dialog', self)
+        self.btn.clicked.connect(self.showDialog)
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Font dialog')
+        self.show()
+
+    def showDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+        print(fname)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
