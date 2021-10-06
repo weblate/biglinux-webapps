35347ee1a6e680f72e2d96458e36ab222bf74db2
eltonff <eltonfabricio10@gmail.com>
2021-10-06 20:03:49 -0300

    ok

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/icon.sh.py
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/icon.sh.py
@@ -1,17 +1,26 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
+
 from PySide2.QtWidgets import QFileDialog, QApplication
+from PySide2.QtGui import QIcon
 import sys
 
 
 class Dialog(QFileDialog):
     def __init__(self):
-        super().__init__()
+        super(Dialog, self).__init__()
 
-        fname = self.getOpenFileName(None, 'Open file', '/home')
-        if fname:
-            print(fname[0])
-        sys.exit()
+        self.setWindowIcon(QIcon.fromTheme('insert-image'))
 
-app = QApplication(sys.argv)
-Dialog()
-app.exec_()
+        fname = self.getOpenFileName(None, 'Images', QIcon.themeSearchPaths()[2], 'Images (*.png *.xpm *.jpg)')
+        file_name = fname[0]
+        if file_name:
+            print(file_name)
+            sys.exit()
+        else:
+            sys.exit()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    Dialog()
+    app.exec_()
