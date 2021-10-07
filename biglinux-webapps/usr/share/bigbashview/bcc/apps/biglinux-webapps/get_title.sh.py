d83688152608072fb9a334fa264748db2e0c18d4
eltonff <eltonfabricio10@gmail.com>
2021-10-07 18:23:36 -0300

    ok

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_title.sh.py
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_title.sh.py
@@ -7,8 +7,12 @@ from bs4 import BeautifulSoup
 url = str(sys.argv[1])
 if 'https' not in url:
     url = 'https://'+url
-r = requests.get(url)
-html_parse = BeautifulSoup(r.content, 'html.parser')
-html_title = html_parse.title.string.strip()
-title = html_title.replace('  ', ' ')
-print(title)
+try:
+    r = requests.get(url)
+    html_parse = BeautifulSoup(r.content, 'html.parser')
+    html_title = html_parse.title.string.strip()
+    title = html_title.replace('  ', ' ')
+    _title = title.replace('|', '')
+    print(title)
+except:
+    sys.exit()
