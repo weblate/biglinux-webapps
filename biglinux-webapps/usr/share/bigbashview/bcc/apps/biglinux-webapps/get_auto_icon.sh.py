4c7b73d20b1beef91f23cd823bb7ae2e35df841a
eltonff <eltonfabricio10@gmail.com>
2021-10-07 22:47:58 -0300

    ok

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_auto_icon.sh.py
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_auto_icon.sh.py
@@ -6,11 +6,11 @@ from gi.repository import Gtk
 from urllib.parse import urlparse
 
 def url_parse(url):
-    url_http = url if url in 'https://' else 'https://'+url
+    url_http = url if 'https://' in url else 'https://'+url
     _url = urlparse(url_http)
-    return _url
     _url_split = _url.netloc.split('.')[0]
-    name = _url_split if _url_split not in ('webz', 'webk', 'web', 'www') else _url.netloc.split('.')[1]
+    matches = ['webz', 'webk', 'web', 'www']
+    name = _url_split if not any(x in _url_split for x in matches) else _url.netloc.split('.')[1]
     return name
 
 def get_icon(icon_name):
@@ -24,5 +24,5 @@ def get_icon(icon_name):
         sys.exit()
 
 if __name__ == "__main__":
-    #get_icon(url_parse(sys.argv[1]))
-    print(url_parse(sys.argv[1]))
+    get_icon(url_parse(sys.argv[1]))
+    #print(url_parse(sys.argv[1]))
--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_favicon.sh.py
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_favicon.sh.py
@@ -9,28 +9,27 @@ if 'https' not in url:
     url = 'https://'+url
 
 icons = favicon.get(url)
-icon = icons[0]
-
-del_https_url = url.replace('https://', '')
-split_url = del_https_url.split('.')
-split_url_first = split_url[0]
-
-if split_url_first == 'www':
-    name_file = split_url[1]
-elif split_url_first == 'web':
-    name_file = split_url[1]
+if len(icons) > 1:
+    for i in icons:
+         print(i.url)
 else:
-    name_file = split_url_first
+    print(icons[0].url)
 
-try:
-    resp = requests.get(icon.url, stream=True)
-    with open('/tmp/{}.{}'.format(name_file, icon.format), 'wb') as img:
-         img.write(resp.content)
-    print('/tmp/{}.{}'.format(name_file, icon.format))
-except:
-    sys.exit()
-
-
-
-#for i in icons:
-#    print(i.url)
+#del_https_url = url.replace('https://', '')
+#split_url = del_https_url.split('.')
+#split_url_first = split_url[0]
+#
+#if split_url_first == 'www':
+#   name_file = split_url[1]
+#elif split_url_first == 'web':
+#   name_file = split_url[1]
+#else:
+#   name_file = split_url_first
+#
+#try:
+#   resp = requests.get(icon.url, stream=True)
+#   with open('/tmp/{}.{}'.format(name_file, icon.format), 'wb') as img:
+#        img.write(resp.content)
+#   print('/tmp/{}.{}'.format(name_file, icon.format))
+#except:
+#   sys.exit()
