ecf434523ec0517cf077520baec222592f385d37
eltonff <eltonfabricio10@gmail.com>
2021-10-22 22:44:59 -0300

    ok

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/enable-disable.sh
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/enable-disable.sh
@@ -4,10 +4,10 @@ if [ ! -e "$HOME/.local/share/applications/$1" ]; then
     cp /usr/share/biglinux/webapps/$1 $HOME/.local/share/applications
     update-desktop-database -q $HOME/.local/share/applications
     kbuildsycoca5 &> /dev/null
-    exit
+    echo 'on'
 else
     rm "$HOME/.local/share/applications/$1"
     update-desktop-database -q $HOME/.local/share/applications
     kbuildsycoca5 &> /dev/null
-    exit
+    echo 'off'
 fi
