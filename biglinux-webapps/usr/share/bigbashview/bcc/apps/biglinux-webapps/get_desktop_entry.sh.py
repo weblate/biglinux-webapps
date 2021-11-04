7318a81f77079dc818aa27d30daf4e74f4ed30e1
eltonff <eltonfabricio10@gmail.com>
2021-11-04 15:19:29 -0300

    Update get_desktop_entry.sh.py

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_desktop_entry.sh.py
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/get_desktop_entry.sh.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
-from xdg.DesktopEntry import DesktopEntry
-from xdg import BaseDirectory
-import glob
+from xdg import DesktopEntry, BaseDirectory
+import glob, json
 
 HOME = BaseDirectory.xdg_data_home
-DIR = glob.glob(HOME+'/applications/*custom.desktop')
+files = glob.glob(HOME+'/applications/*custom.desktop')
 
-for f in DIR:
-    FileEntry = DesktopEntry(f)
-    print(FileEntry.getName(), end='\n\n')
-    print(FileEntry.getIcon(), end='\n\n')
-    print(FileEntry.getExec(), end='\n\n\n\n')
+for f in files:
+    filedesk = DesktopEntry.DesktopEntry(f)
+    fileexec = filedesk.getExec()
+
+    if not fileexec.startswith('/home'):
+        _execfile = fileexec.split()[3].replace('--app=', '')
+    else:
+        with open(fileexec, 'r') as fx:
+            line = fx.readlines()[39]
+            _execfile = line.split()[8].replace('"', '')
+
+    x = { "Name" : filedesk.getName(), "Exec" : _execfile, "Icon" : filedesk.getIcon()}
+    print(json.dumps(x))
