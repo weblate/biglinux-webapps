73d088d3b2e63b35c5feb17869169936572a824a
eltonff <eltonfabricio10@gmail.com>
2021-11-22 15:11:45 -0300

    ok

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/webapp-remove.sh
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/webapp-remove.sh
@@ -4,47 +4,40 @@
 export TEXTDOMAINDIR="/usr/share/locale"
 export TEXTDOMAIN=biglinux-webapps
 
-echo "<font color=white>$filedesk"
+echo "$filedesk"
 exit
 
+NAMEDESK="$(basename -s .desktop "$filedesk" | sed 's|-webapp-biglinux-custom||')"
+ICONDESK="$(grep "Icon=" $filedesk | sed 's|Icon=||')"
+DESKNAME="$(grep "Name=" $filedesk | sed 's|Name=||')"
+
+if [ "$(grep "firefox$" $filedesk)" != "" ];then
+
+    if [ -d $HOME/.bigwebapps/"$NAMEDESK" ]; then
+        rm -r $HOME/.bigwebapps/"$NAMEDESK"
+    fi
+    unlink "$(xdg-user-dir DESKTOP)/$DESKNAME.desktop" &> /dev/null
+    rm "$(grep "Exec=" "$filedesk" | sed 's|Exec=||')"
+    xdg-desktop-menu uninstall "$filedesk"
+    rm "$ICONDESK"
+else
+    if [ -d $HOME/.bigwebapps/"$NAMEDESK" ]; then
+        rm -r $HOME/.bigwebapps/"$NAMEDESK"
+    fi
+    unlink "$(xdg-user-dir DESKTOP)/$DESKNAME.desktop" &> /dev/null
+    xdg-desktop-menu uninstall "$filedesk"
+    rm "$ICONDESK"
+fi
+
+nohup update-desktop-database -q $HOME/.local/share/applications &
+nohup kbuildsycoca5 &> /dev/null &
 kdialog --title "BigLinux WebApps" --icon "internet-web-browser" \
-        --yesno $"Você tem certeza que deseja remover este WebApp?"
+--yesno $"O WebApp foi removido com sucesso!\nVocê deseja remover outro WebApp?"
 
 if [ "$?" != "0" ]; then
-    echo "<META http-equiv=\"refresh\" content=\"0;URL=index-remove.sh.htm\">"
+    echo "<META http-equiv=\"refresh\" content=\"0;URL=index.sh.htm\">"
+    exit
 else
-    NAMEDESK="$(basename -s .desktop "$filedesk" | sed 's|-webapp-biglinux-custom||')"
-    ICONDESK="$(grep "Icon=" $filedesk | sed 's|Icon=||')"
-    DESKNAME="$(grep "Name=" $filedesk | sed 's|Name=||')"
-
-    if [ "$(grep "firefox$" $filedesk)" != "" ];then
-
-        if [ -d $HOME/.bigwebapps/"$NAMEDESK" ]; then
-            rm -r $HOME/.bigwebapps/"$NAMEDESK"
-        fi
-        unlink "$(xdg-user-dir DESKTOP)/$DESKNAME.desktop" &> /dev/null
-        rm "$(grep "Exec=" "$filedesk" | sed 's|Exec=||')"
-        xdg-desktop-menu uninstall "$filedesk"
-        rm "$ICONDESK"
-    else
-        if [ -d $HOME/.bigwebapps/"$NAMEDESK" ]; then
-            rm -r $HOME/.bigwebapps/"$NAMEDESK"
-        fi
-        unlink "$(xdg-user-dir DESKTOP)/$DESKNAME.desktop" &> /dev/null
-        xdg-desktop-menu uninstall "$filedesk"
-        rm "$ICONDESK"
-    fi
-
-    nohup update-desktop-database -q $HOME/.local/share/applications &
-    nohup kbuildsycoca5 &> /dev/null &
-    kdialog --title "BigLinux WebApps" --icon "internet-web-browser" \
-    --yesno $"O WebApp foi removido com sucesso!\nVocê deseja remover outro WebApp?"
-
-    if [ "$?" != "0" ]; then
-        echo "<META http-equiv=\"refresh\" content=\"0;URL=index.sh.htm\">"
-        exit
-    else
-        echo "<META http-equiv=\"refresh\" content=\"0;URL=index-remove.sh.htm\">"
-        exit
-    fi
+    echo "<META http-equiv=\"refresh\" content=\"0;URL=index-remove.sh.htm\">"
+    exit
 fi
